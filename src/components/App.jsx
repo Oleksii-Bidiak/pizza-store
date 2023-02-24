27a1283efc29ef1b1467682ccde1012a5a4009cb
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-02-24 12:37:18 +0200

    Create fetch function fot pizza

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './app.scss'
 import Categories from './categories/Categories'
 import Header from './header/Header'
@@ -5,6 +7,23 @@ import PizzaBlock from './pizzaBlock/PizzaBlock'
 import Sort from './sort/Sort'
 
 function App() {
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const fetchItems = async () => {
+        const response = await axios.get(
+            'https://63f7a28ce40e087c959331c6.mockapi.io/items'
+        )
+        if (response.status === 200) {
+            setIsLoading(false)
+            setItems(response.data)
+        }
+    }
+
+    useEffect(() => {
+        fetchItems()
+    }, [])
+
     return (
         <div class="wrapper">
             <Header />
@@ -16,7 +35,13 @@ function App() {
                     </div>
                     <h2 class="content__title">Все пиццы</h2>
                     <div class="content__items">
-                        <PizzaBlock />
+                        {isLoading ? (
+                            <h1>Завантаження...</h1>
+                        ) : (
+                            items.map(item => (
+                                <PizzaBlock key={item.id} {...item} />
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
--- src/components/pizzaBlock/PizzaBlock.jsx
+++ src/components/pizzaBlock/PizzaBlock.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './pizzaBlock.scss'
 
 export default function PizzaBlock() {
